6691d53dd3f20418c63f5cffe39b43a2032856be
PhucNguyen2812 <1150080071@sv.hcmunre.edu.vn>
2025-10-22 00:38:01 +0700

    add

---
+++ quanlykhoa/src/main/java/edu/kcntt/quanlykhoa/QuanlykhoaApplication.java
@@ -0,0 +1,13 @@
+package edu.kcntt.quanlykhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlykhoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlykhoaApplication.class, args);
+	}
+
+}
---
+++ quanlykhoa/src/test/java/edu/kcntt/quanlykhoa/QuanlykhoaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kcntt.quanlykhoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlykhoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
